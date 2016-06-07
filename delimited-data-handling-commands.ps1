0c825554ac332ef5ad0a06464908f32d68e0e668
Raj Julha <raj.julha@gmail.com>
2016-06-07 19:49:03 +0100

    Add sort command and illustrate addition of properties
    Convert one of the columns into another format and use the result to perform sorting

--- delimited-data-handling-commands.ps1
+++ delimited-data-handling-commands.ps1
@@ -10,3 +10,16 @@ http://stackoverflow.com/questions/3473357/quick-way-to-read-a-tab-delimited-fil
 Get-Content -Path C:\top\sub1\TabDelimfile.txt | ConvertFrom-Csv -Delimiter "`t" | Out-GridView
 
 Import-Csv -Path C:\top\sub1\TabDelimfile.txt -Delimiter "`t" | Out-GridView
+
+Import-Csv -Path C:\top\sub1\TabDelimfile.txt -Delimiter "`t" | sort {$_."colname with same space"} | select 'colname with same space', 'Col A/B sample'
+
+Import-Csv -Path C:\top\sub1\TabDelimfile.txt -Delimiter "`t" | sort {$_."colname with same space"} | select 'colname with same space', 'Col A/B sample'
+
+
+http://serverfault.com/questions/141923/importing-from-csv-and-sorting-by-date
+The sample from the above url demonstrates how we can add proeprties on the fly and also perform field transformations
+such as conversion to DateTime for sorting
+ft below is short form of Format-Table
+
+Import-CSV -delimiter "`t" Output.tab | Where-Object {$_.'First Name' -like '*And*'} | add-member scriptproperty -name 'HireDateTyped' -value { [DateTime]::Parse($this.'Hire Date') } -passthru | Sort-Object 'HireDateTyped' | ft 'Hire Date', 'First Name'
+ 
