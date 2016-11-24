f8780a8a788fc0d0614ca03c199282420db7d2b6
Raj Julha <raj.julha@gmail.com>
2016-11-24 11:50:36 +0400

    Update misc-cmds.ps1

--- misc-cmds.ps1
+++ misc-cmds.ps1
@@ -2,3 +2,11 @@
 2016-11-24
 This file will hold powershell commands as I find them before they go into their specific modules
 #>
+
+# Search text in a list of files
+
+get-childitem -Path toplevelfolder -Filter *.TXT -Recurse | WHERE { select-string "text to search" $_ } | select fullname 
+
+# Display CSV file contents in a trig-view
+Import-Csv .\filename.csv | Out-GridView 
+
