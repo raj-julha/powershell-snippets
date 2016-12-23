7ed9bbd2b3b2227a5ab1899dc745412ece064ad5
Raj Julha <raj.julha@gmail.com>
2016-12-23 07:47:49 +0000

    Update misc-cmds.ps1
    Add sample Invoke-WebRequest commands

--- misc-cmds.ps1
+++ misc-cmds.ps1
@@ -13,3 +13,15 @@ Import-Csv .\filename.csv | Out-GridView
 # Change extension of many files
 
 get-childitem d:\folderlocation -Filter "*.ext" | Rename-Item -NewName { [io.path]::ChangeExtension($_.FullName, "newext")}
+
+# View a CSV file hosted on a a web server
+[System.Text.Encoding]::ASCII.GetString((Invoke-WebRequest -Uri http://sitename/subfolder/filename.csv).Content) | ConvertFrom-Csv | Out-GridView 
+
+# Download a file from a web server
+Invoke-WebRequest -Uri http://sitename/subfolder/filename.zip -OutFile D:\localfolder\locafilename.zip
+
+# http://stackoverflow.com/questions/17325293/invoke-webrequest-post-with-parameters
+# Invoke-WebRequest -UseBasicParsing http://eaxmple.com/service -ContentType "application/json" -Method POST -Body "{ 'ItemID':3661515, 'Name':'test'}"
+
+$postParams = @{username='me';moredata='qwerty'}
+$resp = Invoke-WebRequest -Uri http://example.com/foobar -Method POST -Body $postParams
