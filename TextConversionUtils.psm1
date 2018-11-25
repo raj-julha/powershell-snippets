131e4b1d8992c79b3839992f8019069f7803d56a
Raj Julha <raj.julha@gmail.com>
2018-11-26 00:26:48 +0400

    Add TextConversionUtils.psm1

---
+++ TextConversionUtils.psm1
@@ -0,0 +1,29 @@
+﻿<#
+#>
+Function ConvertTo-PascalCase
+{
+    [CmdLetBinding()]
+    Param([string]$InputStr)
+
+    $MatchEval = {$args[0].Value.ToUpper()}
+    $Pattern = "^."
+    [System.Text.RegularExpressions.Regex]::Replace($InputStr, $Pattern, $MatchEval)
+}
+
+Function Convert-SnakeCaseToCamelCase
+{
+    [CmdLetBinding()]
+    Param([string]$InputStr)
+
+    $MatchEval = {
+        $args[0].Value.Replace("_", "").ToUpper()
+    }
+    $Pattern = "(_[a-z])+"
+    [System.Text.RegularExpressions.Regex]::Replace($InputStr, $Pattern, $MatchEval)        
+}
+
+Export-ModuleMember -Function ConvertTo-PascalCase
+
+# Usage
+#  Import-Module .\TextConversionUtils.psm1
+# 
