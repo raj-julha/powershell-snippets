d9069d5df783d9f337b8efe86eefcdb67cb13b31
Raj Julha <raj.julha@gmail.com>
2022-08-04 22:04:59 +0400

    Update add-methods-to-jsondata.ps1
    Add sample where input JSON data is an array

--- add-methods-to-jsondata.ps1
+++ add-methods-to-jsondata.ps1
@@ -19,7 +19,7 @@ $SampleMapJson = @"
 }
 "@
 
-$LookupMap = ConvertFrom-Json -InputObject $BatchTypeMapJson
+$LookupMap = ConvertFrom-Json -InputObject $SampleMapJson
 
 $LookupMap.SampleMap.Count
 
@@ -32,8 +32,8 @@ $DeriveNewType = {
     # as no items qualifying filter
 
     $MappedType = $DefaultIfNotFound # $InputType
-    
-    $MatchingType = $this.SampleMap.Where({$InputType -match $_.InputType}, 'First')
+    # Do not use -match below or else teh regexp matches partial data
+    $MatchingType = $this.SampleMap.Where({$InputType -eq $_.InputType}, 'First')
     if($MatchingType.Count -gt 0){
         $MappedType = $MatchingType.OutputType
     }
@@ -45,8 +45,6 @@ $DeriveNewType = {
 
 $LookupMap | Add-Member -Name "GetMappedType" -MemberType ScriptMethod -Value $DeriveNewType
 
-
-
 $LookupMap.GetMappedType()
 
 $LookupMap.GetMappedType("104")
@@ -54,3 +52,41 @@ $LookupMap.GetMappedType("104")
 $LookupMap.GetMappedType("XYZ")
 
 $LookupMap.GetMappedType("ABC", "MUR")
+
+# To remove a dynamically added method
+# $LookupMap.psobject.properties.remove('GetMappedType')
+
+# Sample below shows how to add methods when the JSON data is an array, it starts with [ and not {
+# The JSON data is first converted as an array and that array is used as a property in a newly created PSCustomObject
+
+$SampleMapJson2 = @"
+[
+        {"InputType": "100", "OutputType" : "105"},
+        {"InputType": "102", "OutputType" : "107"},
+        {"InputType": "104", "OutputType" : "109"},
+        {"InputType": "106", "OutputType" : "111"}
+]   
+"@
+
+$TempLookupList = ConvertFrom-Json -InputObject $SampleMapJson2
+$LookupMap2 = [PSCustomObject]@{ LookupList = TempLookupList }
+$ScriptCode = {
+ Param($InputType, $DefaultIfNotFound = $InputType)
+   # The Where filter always returns a list, not null so check for Count equal to 0 to consider
+    # as no items qualifying filter
+
+    $MappedType = $DefaultIfNotFound # $InputType
+    # Do not use -match below or else the regexp matches partial data
+    $MatchingType = $this.LookupList.Where({$InputType -eq $_.InputType}, 'First')
+    if($MatchingType.Count -gt 0){
+        $MappedType = $MatchingType.OutputType
+    }
+    
+    # $RetValue = "Map has $($this.SampleMap.Count) items. Input is $($InputType), mapped is $($MappedType) "
+
+    return $MappedType
+}
+
+$LookupMap2 | Add-Member -Name "GetMappedType" -MemberType ScriptMethod -Value $ScriptCode
+
+$LookupMap2.GetMappedType("104")
