157b38775bc3dcd6d18b0e9da0e5c70032b45b5e
Raj Julha <raj.julha@gmail.com>
2024-01-11 20:40:54 +0400

    Update bulk-search-replace.ps1
    Add a TODO to update bulk-search-replace.ps1

--- bulk-search-replace.ps1
+++ bulk-search-replace.ps1
@@ -3,6 +3,8 @@
  
  Get-ChildItem -Path $SourceFolder -Filter "*.xml" | ForEach-Object {
     $OutputFile = Join-Path $TargetFolder -ChildPath $_.Name
+    # The Out-File command adds a empty line ending with CRLF to the resulting file
+    # Review and update
     (Get-Content $_.FullName -Raw) -replace "2022-01-28", "2022-02-23" | Out-File -FilePath $OutputFile -Encoding utf8 
  }
 
